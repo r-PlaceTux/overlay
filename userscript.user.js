72d6cbf3802db73fb74237b770b499014d2f91a7
Epik <epik.est@pm.me>
2022-04-01 23:20:52 +0000

    licensed

--- userscript.user.js
+++ userscript.user.js
@@ -4,6 +4,7 @@
 // @version      1.0.0
 // @description  FLOSS forever!
 // @author       r/PlaceTux
+// @license      GPL 3.0
 // @match        https://hot-potato.reddit.com/embed*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @grant        none
