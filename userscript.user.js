8e74ad37456ab1942f0cdc081f38a159a5f39704
Epik <epik.est@pm.me>
2022-04-01 22:30:52 +0000

    (hopefully) fixed broken link

--- userscript.user.js
+++ userscript.user.js
@@ -13,7 +13,7 @@ if (window.top !== window.self) {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/r-PlaceTux/Overlay/main/tuxoverlay.png?token=GHSAT0AAAAAABTEJ5FMWWUQIIB3J4ZDLVSMYSHPSWQ";
+            i.src = "https://github.com/r-PlaceTux/Overlay/raw/main/tuxoverlay.png";
             i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
             console.log(i);
             return i;
