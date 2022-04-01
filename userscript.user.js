ec51090c7546718530b69a55a74c3a715e8bfdce
James Gregory <77238511+bobgregory@users.noreply.github.com>
2022-04-01 23:26:59 +0100

    Create userscript.user.js

---
+++ userscript.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         osu! Logo template
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the canvas!
+// @author       oralekin
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://cdn.discordapp.com/attachments/958517233485746228/959576116023140362/tuxoverlay.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 1000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
