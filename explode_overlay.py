66992289edfd1154769c6351822d18a38a1ce15f
ryleu <riley.j.mclain@gmail.com>
2022-04-02 14:56:44 -0400

    first test of workflow

---
+++ explode_overlay.py
@@ -0,0 +1,24 @@
+#!/bin/env python3
+
+import matplotlib.pyplot as plt
+import numpy
+import os
+
+os.mkdir("out")
+
+p = plt.imread("overlay_unexploded.png")
+
+newImage = numpy.zeros((len(p) * 3, len(p[0]) * 3, 4), dtype=numpy.float32)
+
+
+# convert a point on the true image to a point on the exploded image
+def fix_pos(pos):
+    return pos * 3 + 1
+
+
+for row in range(len(p)):
+    for tile in range(len(p[row])):
+        for i in range(len(p[row][tile])):
+            newImage[fix_pos(row)][fix_pos(tile)][i] = p[row][tile][i]
+
+plt.imsave("out/overlay_exploded.png", newImage, dpi=100, format="png")
