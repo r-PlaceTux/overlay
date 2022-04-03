1c1ff380af0714dfc4917cf3688b0ae13ab6d961
Riley <69326171+ryleu@users.noreply.github.com>
2022-04-02 20:12:28 -0400

    merge with r/cosmere

--- userscript-blurfix.user.js
+++ userscript-blurfix.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         r/PlaceTux Overlay Blur Fix
 // @namespace    https://github.com/r-PlaceTux/Overlay
-// @version      1.0.5
+// @version      1.0.7
 // @description  FLOSS forever!
 // @author       r/PlaceTux
 // @match        https://hot-potato.reddit.com/embed*
@@ -18,6 +18,16 @@ if (window.top !== window.self) {
                 i.style = "position: absolute;left: 0;top: 0;image-rendering: crisp-edges;width: 1000px;height: 1000px;";
                 console.log(i);
                 return i;
+            })())
+    }, false);
+    window.addEventListener('load', () => {
+        document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+            (function () {
+                const j = document.createElement("img");
+		j.src = "https://github.com/Atkion/placeCosmereOverlay/raw/master/template.png";
+		j.style = "position: absolute;left: 0;top: 0;image-rendering: crisp-edges;width: 2000px;height: 1000px;";
+                console.log(j);
+                return j;
             })())
     }, false);
 }
